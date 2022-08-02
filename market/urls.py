044e529f60ff3fadbfd9145db02cfb6730bc3778
L7R4 <lautaro.rodriguez553@gmail.com>
2022-08-01 23:33:52 -0300

    Modificacion del header que ocupe todo el ancho, y creacion de pestañas de las categorias

--- market/urls.py
+++ market/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from market.views import CategoriaMotos, CategoriaElectrodemesticos,CategoriaSolucionesDinerarias,CategoriaBeneficiosCliente
 app_name="market"
 
 urlpatterns = [
-    
+    path("motos/",CategoriaMotos.as_view(),name="motos"),
+    path("elec/",CategoriaElectrodemesticos.as_view(), name="electrodomesticos"),
+    path("beneficios/", CategoriaBeneficiosCliente.as_view(), name="beneficios"),
+    path("soluciones_dinerarias/", CategoriaSolucionesDinerarias.as_view(),name="soluciones_dinerarias")
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
--- market/views.py
+++ market/views.py
@@ -1,3 +1,35 @@
+from configparser import NoSectionError
 from django.shortcuts import render
+from django.views.generic import View
+from django.views import generic
+from market.models import Post
 
-# Create your views here.
+
+class CategoriaMotos(generic.ListView):
+    template_name = "categoria-motos.html"
+    
+    def get_queryset(self):
+        return None
+
+
+class CategoriaElectrodemesticos(generic.ListView):
+    template_name = "categoria-electrodomesticos.html"
+
+    def get_queryset(self):
+        return None
+
+
+class CategoriaSolucionesDinerarias(generic.ListView):
+    template_name = "categoria-soluciones-dinerarias.html"
+
+    def get_queryset(self):
+        return None
+
+
+class CategoriaBeneficiosCliente(generic.ListView):
+    template_name = "categoria-beneficios-cliente.html"
+
+    def get_queryset(self):
+        return None
+
+
