7b7b295b0345406730d71b49a3360cce8f99e6ea
L7R4 <lautaro.rodriguez553@gmail.com>
2022-08-03 22:42:02 -0300

    avanzando en los mismo x3

--- static/js/slider.js
+++ static/js/slider.js
@@ -3,24 +3,28 @@ const itemCount = items.length;
 const nextItem = document.querySelector('.next');
 const previousItem = document.querySelector('.previous');
 var slider = document.querySelector(".slider");
+
 var img = document.querySelectorAll(".slider img");
 let count = 0;
 calcDisplaySlider = -100/itemCount;
 alert(calcDisplaySlider);
 
-img.forEach(function(image){
+img.forEach(function(image,i){
   image.setAttribute("style","width:calc(100%/" + itemCount + ")");
+  console.log(i);
+  nextItem.addEventListener("click",()=>{
+    operacion += i * calcDisplaySlider 
+    slider.style.transform = "translateX("+ operacion +"%)"
+    // if(itemCount< i + 1){
+    //   calcDisplaySlider = 0;
+    // }
+  })
 })
 
-arrayAtributosSlider =["width:"+ itemCount*100 +"%","transform: translateX("+calcDisplaySlider +"%)"]
-function atributosSlider (elem,atributos){
-  for (var i; atributos; i++){ 
-    elem.setAttribute("style",i)
-  }
-}
 
-atributosSlider(slider, arrayAtributosSlider);
-// tamañoSlider = slider.setAttribute("style","width:"+ itemCount*100 +"%");
+// Object.assign(slider.style,{width:itemCount*100 +"%",transform:"translateX(" + calcDisplaySlider +"%)"});
 
-// displaySlider = slider.setAttribute("style","transform: translateX("+calcDisplaySlider +"%)")
+tamañoSlider = slider.setAttribute("style","width:"+ itemCount*100 +"%");
+
+// slider.style.transform = "translateX("+ operacion +"%)"
 
