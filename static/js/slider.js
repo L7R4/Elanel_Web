4cc3277833c2bbf59fcfb172aad8de777b444d1e
L7R4 <lautaro.rodriguez553@gmail.com>
2022-08-03 21:26:11 -0300

    Avanzando con el desplazamiento del slider

--- static/js/slider.js
+++ static/js/slider.js
@@ -2,44 +2,60 @@ const items = document.querySelectorAll('.slider img');
 const itemCount = items.length;
 const nextItem = document.querySelector('.next');
 const previousItem = document.querySelector('.previous');
+var slider = document.querySelector(".slider");
+var img = document.querySelectorAll(".slider img");
 let count = 0;
 
-function showNextItem() {
-  items[count].classList.remove('active');
+img.forEach(function(image){
+  image.setAttribute("style","width:calc(100%/" + itemCount + ")");
+})
 
-  if(count < itemCount - 1) {
-    count++;
-  } else {
-    count = 0;
-  }
+tamañoSlider = slider.setAttribute("style","width:"+ itemCount*100 +"%");
 
-  items[count].classList.add('active');
-  console.log(count);
-}
+// calcDisplaySlider = (itemCount*100)/itemCount;
+// alert(calcDisplaySlider);
 
-function showPreviousItem() {
-  items[count].classList.remove('active');
+// displaySlider = slider.setAttribute("style","transform:")
 
-  if(count > 0) {
-    count--;
-  } else {
-    count = itemCount - 1;
-  }
 
-  items[count].classList.add('active');
-  console.log(count);
-}
 
-function keyPress(e) {
-  e = e || window.event;
+
+// function showNextItem() {
+//   items[count].classList.remove('active');
+
+//   if(count < itemCount - 1) {
+//     count++;
+//   } else {
+//     count = 0;
+//   }
+
+//   items[count].classList.add('active');
+//   console.log(count);
+// }
+
+// function showPreviousItem() {
+//   items[count].classList.remove('active');
+
+//   if(count > 0) {
+//     count--;
+//   } else {
+//     count = itemCount - 1;
+//   }
+
+//   items[count].classList.add('active');
+//   console.log(count);
+// }
+
+// function keyPress(e) {
+//   e = e || window.event;
   
-  if (e.keyCode == '37') {
-    showPreviousItem();
-  } else if (e.keyCode == '39') {
-    showNextItem();
-  }
-}
+//   if (e.keyCode == '37') {
+//     showPreviousItem();
+//   } else if (e.keyCode == '39') {
+//     showNextItem();
+//   }
+// }
 
-nextItem.addEventListener('click', showNextItem);
-previousItem.addEventListener('click', showPreviousItem);
-document.addEventListener('keydown', keyPress);
+// nextItem.addEventListener('click', showNextItem);
+// previousItem.addEventListener('click', showPreviousItem);
+// document.addEventListener('keydown', keyPress);
