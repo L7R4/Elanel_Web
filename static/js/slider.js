820e18b310cc85ad5c86325115b5e82009286026
L7R4 <lautaro.rodriguez553@gmail.com>
2022-08-03 22:02:16 -0300

    avanzando con lo mismo x2

--- static/js/slider.js
+++ static/js/slider.js
@@ -5,57 +5,22 @@ const previousItem = document.querySelector('.previous');
 var slider = document.querySelector(".slider");
 var img = document.querySelectorAll(".slider img");
 let count = 0;
+calcDisplaySlider = -100/itemCount;
+alert(calcDisplaySlider);
 
 img.forEach(function(image){
   image.setAttribute("style","width:calc(100%/" + itemCount + ")");
 })
 
-tamañoSlider = slider.setAttribute("style","width:"+ itemCount*100 +"%");
+arrayAtributosSlider =["width:"+ itemCount*100 +"%","transform: translateX("+calcDisplaySlider +"%)"]
+function atributosSlider (elem,atributos){
+  for (var i; atributos; i++){ 
+    elem.setAttribute("style",i)
+  }
+}
 
-// calcDisplaySlider = (itemCount*100)/itemCount;
-// alert(calcDisplaySlider);
+atributosSlider(slider, arrayAtributosSlider);
+// tamañoSlider = slider.setAttribute("style","width:"+ itemCount*100 +"%");
 
-// displaySlider = slider.setAttribute("style","transform:")
+// displaySlider = slider.setAttribute("style","transform: translateX("+calcDisplaySlider +"%)")
 
-
-
-
-// function showNextItem() {
-//   items[count].classList.remove('active');
-
-//   if(count < itemCount - 1) {
-//     count++;
-//   } else {
-//     count = 0;
-//   }
-
-//   items[count].classList.add('active');
-//   console.log(count);
-// }
-
-// function showPreviousItem() {
-//   items[count].classList.remove('active');
-
-//   if(count > 0) {
-//     count--;
-//   } else {
-//     count = itemCount - 1;
-//   }
-
-//   items[count].classList.add('active');
-//   console.log(count);
-// }
-
-// function keyPress(e) {
-//   e = e || window.event;
-  
-//   if (e.keyCode == '37') {
-//     showPreviousItem();
-//   } else if (e.keyCode == '39') {
-//     showNextItem();
-//   }
-// }
-
-// nextItem.addEventListener('click', showNextItem);
-// previousItem.addEventListener('click', showPreviousItem);
-// document.addEventListener('keydown', keyPress);
