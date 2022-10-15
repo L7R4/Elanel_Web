cbc89f3e8c35add2016158fc0a48c98b3a1b602e
Lautaro <lautaro.rodriguez553@gmail.com>
2022-10-15 17:04:07 -0300

    15-10

--- static/js/message_form.js
+++ static/js/message_form.js
@@ -1,16 +1,3 @@
-const form_product = document.querySelector("#form_product")
+const input_target = document.querySelector("#objetivo")
+console.table(input_target)
 
-form_product.addEventListener("submit", async ()=>{
-    Swal.fire({
-        title: '¡Muchas gracias!',
-        text: 'Nos comunicaremos contigo en un momento',
-        icon: 'success',
-        // color:'red'
-        iconColor: '#027df1',
-        width:'40%',
-        // height:'300px',
-        padding: '5% 5%',
-        confirmButtonText:'Salir',
-      })
-
-})
