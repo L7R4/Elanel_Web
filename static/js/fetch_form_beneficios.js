fd39e2074b82edc895561e99a616302cb2337e9d
Lautaro <lautaro.rodriguez553@gmail.com>
2023-03-24 19:42:00 -0300

    mejorando cosas

--- static/js/fetch_form_beneficios.js
+++ static/js/fetch_form_beneficios.js
@@ -3,58 +3,60 @@ const inputs_form = document.querySelectorAll(".window_form > #form_fetch_post i
 const message_success = document.querySelector(".message_success")
 const message_error = document.querySelector(".message_error")
 
-button_submit.addEventListener("click", EnviarDatos)
 var url = window.location.pathname;
-
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
+window.addEventListener('load',()=>{
+    button_submit.addEventListener("click", EnviarDatos)
+    function getCookie(name) {
+        let cookieValue = null;
+        if (document.cookie && document.cookie !== '') {
+            const cookies = document.cookie.split(';');
+            for (let i = 0; i < cookies.length; i++) {
+                const cookie = cookies[i].trim();
+                // Does this cookie string begin with the name we want?
+                if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                    break;
+                }
             }
         }
+        return cookieValue;
     }
-    return cookieValue;
-}
-
-function EnviarDatos() {
-    var form = new FormData(document.querySelector(".window_form > form"))
-
-    let post = fetch(url,{
-        method: "POST",
-        body: form,
-        headers: {
-            "X-CSRFToken": getCookie('csrftoken')
-        }
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log(data)
-        if (data) {
-            LimpiarDatos()
-            message_error.classList.remove("hide")
-        }
-    }).catch(error => {
-        LimpiarDatos()
-        message_error.classList.add("hide")
-        message_success.classList.remove("hide")
-    })
     
-}
-
-function LimpiarDatos() {
-    for (let i = 1; i < inputs_form.length; i++) {
-        const element = inputs_form[i];
-        element.value = ""
+    function EnviarDatos() {
+        var form = new FormData(document.querySelector(".window_form > form"))
+    
+        let post = fetch(url,{
+            method: "POST",
+            body: form,
+            headers: {
+                "X-CSRFToken": getCookie('csrftoken')
+            }
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            if (data) {
+                LimpiarDatos()
+                message_error.classList.remove("hide")
+            }
+        }).catch(error => {
+            LimpiarDatos()
+            message_error.classList.add("hide")
+            message_success.classList.remove("hide")
+        })
         
     }
-    // inputs_form.forEach(element => {
-    //     element.value = ""
-    // });
-    button_submit.classList.add("desactive")
-}
+    
+    function LimpiarDatos() {
+        for (let i = 1; i < inputs_form.length; i++) {
+            const element = inputs_form[i];
+            element.value = ""
+            
+        }
+        // inputs_form.forEach(element => {
+        //     element.value = ""
+        // });
+        button_submit.classList.add("desactive")
+    }
+
+})
--- static/js/modal_form.js
+++ static/js/modal_form.js
@@ -11,37 +11,40 @@ const monto_value = document.querySelector("#monto");
 const service_selected = document.querySelector(".service_selected")
 const product_selected = document.querySelector(".product_selected")
 const monto_selected = document.querySelector(".monto_selected")
-// console.log(clearMonto("$2000/mes"))
-open_modal.forEach(element => {
-    element.addEventListener("click",()=>{
-        background_modal.classList.add("active_modal");
-        modal_window.classList.add("active_modal");
-        plan_value.value = element.parentElement.childNodes[1].textContent
-        product_value.value = element.parentElement.children[3].children[0].children[0].textContent
-        monto_value.value = clearMonto(element.parentElement.children[2].textContent) 
-        // monto_value.value = Math.trunc(monto_del_producto)
-        service_selected.innerText = plan_value.value
-        product_selected.innerText = product_value.value
-        monto_selected.innerText = "$" + monto_value.value + "/mes"
+
+window.addEventListener('load',()=>{
+    open_modal.forEach(element => {
+        element.addEventListener("click",()=>{
+            background_modal.classList.add("active_modal");
+            modal_window.classList.add("active_modal");
+            plan_value.value = element.parentElement.childNodes[1].textContent
+            product_value.value = element.parentElement.children[3].children[0].children[0].textContent
+            monto_value.value = clearMonto(element.parentElement.children[2].textContent) 
+            // monto_value.value = Math.trunc(monto_del_producto)
+            service_selected.innerText = plan_value.value
+            product_selected.innerText = product_value.value
+            monto_selected.innerText = "$" + monto_value.value + "/mes"
+        })
+    });
+    
+    close_modal.addEventListener("click",()=>{
+        background_modal.classList.remove("active_modal");
+        modal_window.classList.remove("active_modal");
+        message_success.classList.add("hide")
+        message_error.classList.add("hide")
     })
-});
-
-close_modal.addEventListener("click",()=>{
-    background_modal.classList.remove("active_modal");
-    modal_window.classList.remove("active_modal");
-    message_success.classList.add("hide")
-    message_error.classList.add("hide")
-})
-
-function clearMonto(monto){
-    const numbers = ["0","1","2","3","4","5","6","7","8","9"]
-    let number_clear = ""
-
-    for (let i = 0; i < monto.length; i++) {
-        if (monto[i] in numbers) {
-            number_clear += monto[i]
+    
+    function clearMonto(monto){
+        const numbers = ["0","1","2","3","4","5","6","7","8","9"]
+        let number_clear = ""
+    
+        for (let i = 0; i < monto.length; i++) {
+            if (monto[i] in numbers) {
+                number_clear += monto[i]
+            }
         }
+        return parseInt(number_clear)
     }
-    return parseInt(number_clear)
-}
+
+})
 
