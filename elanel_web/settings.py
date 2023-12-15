f03611f1866a1761cb370c933b050c333a3b4db3
Lautaro <lautaro.rodriguez553@gmail.com>
2023-12-15 01:36:03 -0300

    act video y nuevas sucursales

--- elanel_web/settings.py
+++ elanel_web/settings.py
@@ -120,12 +120,12 @@ DATA_UPLOAD_MAX_NUMBER_FIELDS = None
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-#DATABASES = {
+# DATABASES = {
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
-#}
+# }
 # DATABASES = {
 #    'default': {
 #        'ENGINE': os.environ.get('ENGINE'),
